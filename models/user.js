02a71d76e8a2d818269d957e444cc97d7e1b2698
Ieva Kazaka <ieva.kazaka@gmail.com>
2019-01-29 13:50:04 +0000

    changed register data

--- models/user.js
+++ models/user.js
@@ -4,9 +4,6 @@ const passportLocalMongoose = require("passport-local-mongoose");
 let UserSchema = new mongoose.Schema({
     username: {type: String, unique: true, required: true},
     password: String,
-    avatar: String,
-    firstName: String,
-    lastName: String,
     email: {type: String, unique: true, required: true},
     resetPasswordToken: String,
     resetPasswordExpires: Date,
